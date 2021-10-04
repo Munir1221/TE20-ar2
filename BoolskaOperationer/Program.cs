53774f8094a109a2bf13af2a78d08c42c9773994
monir.abdalla@ga.ntig.se <monir.abdalla@ga.ntig.se>
2021-10-04 14:46:52 +0200

    klar med min berättelse

---
+++ BoolskaOperationer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BoolskaOperationer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Åldersgränser i sverige");
+
+            System.Console.Write("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            //Om man är 15 och äldre
+            if (ålder >= 15)
+            {
+               Console.WriteLine("Du får köra moped"); 
+            }
+
+            // Om man är 18 eller äldre
+           if (ålder >= 18)
+           {
+               Console.WriteLine("Du får köra bil");
+           } 
+
+
+            {
+                
+            }
+            {
+                
+            }
+        }
+    }
+}
---
+++ berättelse/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace berättelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Är du vuxen? (yes/no)");
+            string vuxen = Console.ReadLine();
+
+            if (vuxen == "yes")
+            {
+               Console.WriteLine("Har du tagit studentexamen? (yes/no)"); 
+               string examen = Console.ReadLine();
+               if (examen == "yes")
+               {
+                   Console.WriteLine("Har du ett jobb?");
+                   string job = Console.ReadLine();
+                   if (job == "no")
+                   {
+                       Console.WriteLine("Försök att ta studentexamen? (yes/no)");
+                   }
+                   else
+                   {
+                       Console.WriteLine("Vill du ha ett jobb? (yes/no)");
+                
+                   }
+               }
+               else
+               {
+                   Console.WriteLine("Vill du göra det? (yes/no)");
+                   string student = Console.ReadLine();
+                
+               }
+            }
+            else
+            {
+                Console.WriteLine("Har du en vuxen bredvid dig? yes/no");
+                 vuxen = Console.ReadLine();
+                if (vuxen == "no" )
+                {
+                    Console.WriteLine("Du behöver en vuxen för att svara på följande frågor");
+                }
+                else
+                {
+                    Console.WriteLine("Svara på frågorna noggrant");
+                }
+            }
+            
+        }
+    }
+}
